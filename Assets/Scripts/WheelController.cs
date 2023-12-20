258b97aa9967f07dddc5617549535b4425d68735
PeckishDino <peckishdino@hotmail.com>
2023-12-20 23:10:43 +0000

    city and SOUND

--- Assets/Scripts/WheelController.cs
+++ Assets/Scripts/WheelController.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WheelController : MonoBehaviour
+public class Taxi : MonoBehaviour
 {
     [SerializeField] WheelCollider FR, FL, RR, RL; //wheels (Front Right, Left, Rear Right, Left)
     [SerializeField] Transform FRT, FLT, RRT, RLT; //T = Transform
-    [SerializeField] private float maxSpeed = 5000f;
+    [SerializeField] private float maxSpeed = 10f;
 
 
     public float acceleration = 1000f;
@@ -19,14 +19,27 @@ public class WheelController : MonoBehaviour
     private float currentBrakeForce = 0f;
     private float currentTurnAngle = 0f;
 
+    private AudioSource carIdle;
+    private AudioSource carRev;
+
+    private void Start()
+    {
+        carIdle = GetComponent<AudioSource>();
+        carRev = GetComponent<AudioSource>();
+    }
     private void FixedUpdate()
     {
         currentAcceleration = acceleration * Input.GetAxis("Vertical");
 
         speed = GetComponent<Rigidbody>().velocity.magnitude;
-        if (speed >= maxSpeed)
+
+        if (speed > maxSpeed)
         {
-            currentAcceleration = 0f;
+            // Calculate the capped velocity
+            Vector3 cappedVelocity = GetComponent<Rigidbody>().velocity.normalized * maxSpeed;
+
+            // Set the car's velocity to the capped value
+            GetComponent<Rigidbody>().velocity = cappedVelocity;
         }
 
         if (Input.GetKey(KeyCode.Space))
@@ -51,6 +64,15 @@ public class WheelController : MonoBehaviour
         UpdateWheel(FL, FLT);
         UpdateWheel(RR, RRT);
         UpdateWheel(RL, RLT);
+
+        if (Input.GetAxis("Vertical") != 0f && !carRev.isPlaying)
+        {
+            carRev.Play();
+        }
+        else if (Input.GetAxis("Vertical") == 0f)
+        {
+            carRev.Stop();
+        }
     }
 
     void UpdateWheel(WheelCollider col, Transform trans)
